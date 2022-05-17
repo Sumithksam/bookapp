c54e27158dc163af652ffee7e0b5737fe871936a
Sumithksam <sumith77sam@gmail.com>
2022-05-17 14:08:46 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import Addbooks from './components/Addbooks';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Searchbook from './components/Searchbook';
+import Navbar1 from './components/Navbar1';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-    <Searchbook/>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" exact element={<Addbooks/>}/>
+      <Route path="/search" exact element={<Searchbook/>}/>
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
